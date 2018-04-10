4cb4142138b06b2c9929c25f228653a980ad65d6
volkovm <max.volkov@intelliware.ca>
2018-04-10 15:22:07 -0400

    working now, dirty

--- spring-struts/src/main/java/org/apache/struts/edit/SpringApplicationConfiguration.java
+++ spring-struts/src/main/java/org/apache/struts/edit/SpringApplicationConfiguration.java
@@ -3,10 +3,12 @@ package org.apache.struts.edit;
 import org.apache.struts.edit.service.EditController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+//@Import(MessageConfig.class)
 public class SpringApplicationConfiguration {
 
     @Bean
